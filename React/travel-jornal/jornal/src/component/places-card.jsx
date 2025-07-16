b0138b44df883d3eb27d1bad294a86006e182a6a
aar7-7 <aliasgharrahimi34@gmail.com>
2025-07-16 21:13:25 +0330

    card component layout changed.

--- React/travel-jornal/jornal/src/component/places-card.jsx
+++ React/travel-jornal/jornal/src/component/places-card.jsx
@@ -1,33 +1,29 @@
 import '../style/card.css'
 export default function Card(props){
   console.log(props);
-    return (
-      <>
-        <div className="card"> 
-          <div className="location-poster">
-            <img
-              src={props.img.src}
-              alt={props.img.alt}
-              className="location-img"
-            />
-          </div>
-          <div className="location-detail">
-            <div className="location">
-              <img
-                src="../src/assets/marker.png"
-                alt="marker image"
-                className="marker"
-              />
-              <span>{props.title}</span>
-              <a href={props.googleMapsLink}>View on GoogleMaps</a>
-            </div>
-            <h2 className="location-name">{props.country}</h2>
-            <div className="travel-date">{props.date}</div>
-            <div className="travel-description">
-              {props.text}
-            </div>
-          </div>
+  return (
+    <div className="card">
+      <img src={props.img.src} alt={props.img.alt} className="card-img" />
+      <div className="card-content">
+        <div className="location">
+          <img
+            src="../src/assets/marker.png"
+            alt="marker"
+            className="location-icon"
+          />
+          <span>{props.title}</span>
+          <a
+            href={props.googleMapsLink}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            View on Google Maps
+          </a>
         </div>
-      </>
-    );
+        <h2 className="location-name">{props.country}</h2>
+        <div className="date">{props.date}</div>
+        <p className="description">{props.text}</p>
+      </div>
+    </div>
+  );
 }
