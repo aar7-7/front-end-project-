acc3a502f57cf960351bd354c98f02fd82e679a8
aar7-7 <aliasgharrahimi34@gmail.com>
2025-07-18 16:30:28 +0330

    props updated

--- React/travel-jornal/jornal/src/App.jsx
+++ React/travel-jornal/jornal/src/App.jsx
@@ -3,19 +3,7 @@ import Card from './component/places-card.jsx'
 import Data from "./data/travel-data.js";
 function App() {
   const travelDataElement = Data.map((data) => {
-    return (
-      <Card
-        img={{
-          src: data.img.src,
-          alt: data.img.alt,
-        }}
-        title={data.title}
-        country={data.country}
-        googleMapsLink={data.googleMapsLink}
-        date={data.dates}
-        text={data.text}
-      />
-    );
+    return <Card {...data} />;
   });
 
   return (
