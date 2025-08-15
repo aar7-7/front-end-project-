e84d2af0697c5c082cda82a819f9661a71d4b566
aar7-7 <aliasgharrahimi34@gmail.com>
2025-08-15 19:41:52 +0330

    feat: implement ingredient list and recipe suggestion components

--- React/chef-claude/src/component/body.jsx
+++ React/chef-claude/src/component/body.jsx
@@ -1,33 +1,37 @@
 import React from "react";
-export default function Main(){
+import CludeRecipe from "./cludeRecipe";
+import IngredientsList from "./ingredientsList";
+export default function Main() {
+  const [ingredients, setIngredients] = React.useState([
+    "all the main spices",
+    "pasta",
+    "ground beef",
+  ]);
+  const [recipeShown, setRecipeShown] = React.useState(false);
 
-    const [ingredients , setIngredients] = React.useState([])
-    const ingredienstsListItem = ingredients.map(
-        ingredient => (<li key={ingredient}>{ingredient} </li>))
-    function addIngredient(formData) {
-      const newIngredient = formData.get("ingredient");
-      setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
-    }
-    return (
-      <>
-        <div className="main">
-          <form className="ingredient-form" action={addIngredient}>
-            <input type="text" placeholder="e.g Oregano" name="ingredient" />
-            <button>Add ingredient</button>
-          </form>
+  function addIngredient(formData) {
+    const newIngredient = formData.get("ingredient");
+    setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
+  }
+  function flipResipeShown() {
+    setRecipeShown((prevRecipeShown) => !prevRecipeShown);
+  }
 
-          <div className="ingredients-on-hand">
-            <h2>ingredients on hand:</h2>
-            <ul>{ingredienstsListItem}</ul>
-            <div className="get-recipe-suggestion">
-              <div>
-                <h3>Ready for a recipe?</h3>
-                <p>Generate a recipe from your list of ingredients.</p>
-              </div>
-              <button>Get a recipe</button>
-            </div>
-          </div>
-        </div>
-      </>
-    );
+  return (
+    <>
+      <div className="main">
+        <form className="ingredient-form" action={addIngredient}>
+          <input type="text" placeholder="e.g Oregano" name="ingredient" />
+          <button>Add ingredient</button>
+        </form>
+        {ingredients.length > 0 ? (
+          <IngredientsList
+            ingredients={ingredients}
+            flipResipeShown={flipResipeShown}
+          />
+        ) : null}
+        {recipeShown ? <CludeRecipe /> : null}
+      </div>
+    </>
+  );
 }
---
+++ React/chef-claude/src/component/ingredientsList.jsx
@@ -0,0 +1,20 @@
+export default function IngredientsList(props){
+    const ingredienstsListItem = props.ingredients.map((ingredient) => (
+      <li key={ingredient}>{ingredient} </li>
+    ));
+    return (
+      <div className="ingredients-on-hand">
+        <h2>ingredients on hand:</h2>
+        <ul>{ingredienstsListItem}</ul>
+        {props.ingredients.length > 3 ? (
+          <div className="get-recipe-suggestion">
+            <div>
+              <h3>Ready for a recipe?</h3>
+              <p>Generate a recipe from your list of ingredients.</p>
+            </div>
+            <button onClick={props.flipResipeShown}>Get a recipe</button>
+          </div>
+        ) : null}
+      </div>
+    );
+}
