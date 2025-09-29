db9393fd634c83b4b22c134ffcac62dcf5b9403b
aar7-7 <aliasgharrahimi34@gmail.com>
2025-09-29 21:20:09 +0330

    final commit, some bug fixed

--- React/tenzies/src/App.jsx
+++ React/tenzies/src/App.jsx
@@ -1,15 +1,24 @@
 import Die from "./components/die.jsx";
 import React from "react";
 import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
+
 function App() {
-  const [dice, setDice] = React.useState(generateAllNewDice());
-  const allHeld = dice.every((die) => die.isHeld);
-  const firstValue = dice[0].value;
-  const allSameValue = dice.every((die) => die.value === firstValue);
-  if (allHeld && allSameValue) {
-    alert("You won!");
+  const [dice, setDice] = React.useState(() => generateAllNewDice());
+  const tenziesCompleted = React.useRef();
+
+  const gameWon =
+    dice.every((die) => die.isHeld) &&
+    dice.every((die) => die.value === dice[0].value);
+  if (gameWon) {
+    console.log("You won!");
   }
-  
+  React.useEffect(() => {
+    if (gameWon && tenziesCompleted.current) {
+      tenziesCompleted.current.focus();
+    }
+  }, [gameWon]);
+
   function generateAllNewDice() {
     const diceArr = [];
     for (var i = 1; i <= 10; i++) {
@@ -29,11 +38,17 @@ function App() {
     );
   });
   function rollDice() {
-    setDice((oldDice) =>
-      oldDice.map((die) =>
-        die.isHeld ? die : { ...die, value: Math.floor(Math.random() * 6) + 1 }
-      )
-    );
+    if (!gameWon) {
+      setDice((oldDice) =>
+        oldDice.map((die) =>
+          die.isHeld
+            ? die
+            : { ...die, value: Math.floor(Math.random() * 6) + 1 }
+        )
+      );
+    } else {
+      setDice(generateAllNewDice());
+    }
   }
   function hold(id) {
     const clickedDie = dice.find((die) => die.id === id);
@@ -48,10 +63,16 @@ function App() {
 
   return (
     <>
+      {gameWon && <Confetti />}
       <div className="main">
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">
+          Roll until all dice are the same. Click each die to freeze it at its
+          current value between rolls.
+        </p>
         <div className="game-container">{diceElement}</div>
-        <button className="roll-dice" onClick={rollDice}>
-          Roll
+        <button ref={tenziesCompleted} className="roll-dice" onClick={rollDice}>
+          {gameWon ? "New Game" : "Roll"}
         </button>
       </div>
     </>
