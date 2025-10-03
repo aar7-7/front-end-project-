815225536d212ff85ceadab6116aeb65f6bba096
aar7-7 <aliasgharrahimi34@gmail.com>
2025-10-03 12:59:12 +0330

    header completed

---
+++ React/assembly end game/src/App.jsx
@@ -0,0 +1,14 @@
+import './styles/primary-style.css';
+function App() {
+  return (
+    <div className="header">
+      <h1>Assembly: Endgame</h1>
+      <p>
+        Assembly: Endgame Guess the word in under 8 attempts to keep the
+        programming world safe from Assembly!
+      </p>
+    </div>
+  );
+}
+
+export default App;
